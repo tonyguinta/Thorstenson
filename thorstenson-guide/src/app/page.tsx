52e7c31c240512e190c9e07ba5e9b2e34d9385b1
Tony Guinta <tonyguinta@gmail.com>
2025-07-07 05:18:55 -0500

    Test staging deployment with small comment change

--- thorstenson-guide/src/app/page.tsx
+++ thorstenson-guide/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  // STAGING: Testing staging branch deployment
   return (
     <>
       {/* Hero Section */}
